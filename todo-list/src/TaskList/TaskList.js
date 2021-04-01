e0c41bde0fab863b93ea59a88f2445c4445912dc
feychou <federica.recanatini@gmail.com>
2021-04-01 11:41:36 +0200

    save tasks to local storage

--- todo-list/src/TaskList/TaskList.js
+++ todo-list/src/TaskList/TaskList.js
@@ -1,24 +1,41 @@
 //franzi is working here(-:)
 
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import NewTaskInput from "../NewTaskInput";
 import Task from "../Task";
 import "./TaskList.css";
 
 
 function TaskList() {
+    // default to local storage if something's there, otherwise []
+    const [tasks, setTasks] = useState(() => {
+      try {
+        // Get from local storage by key
+        const items = window.localStorage.getItem('tasks');
+        // Parse stored json or if none return initialValue
+        return items ? JSON.parse(items) : [];
+      } catch (error) {
+        // If error also return initialValue
+        console.log(error);
+        return [];
+      }
+  });
 
-    const [tasks, setTasks] = useState([]);                       //inital state 
-    
+    // useEffect to observe changes in tasks list
+    useEffect(() => {
+      try {
+        // Save to local storage
+        window.localStorage.setItem('tasks', JSON.stringify(tasks));
+      } catch (error) {
+        console.log(error);
+      }
+    }, [tasks])
+
     const addTask = task => {
-
-    const newTasks=[task, ...tasks]
-
-    setTasks(newTasks)
+      const newTasks=[task, ...tasks]
+      setTasks(newTasks)
     };
-    // part 1 done
 
-//  edit section
     const updateTask = (taskId, newValue) => {
      setTasks(prev => prev.map(item => (item.id === taskId ? newValue :item )));
     };
